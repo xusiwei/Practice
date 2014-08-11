11901192eb95e66a02ebd5e40f9019e270882c6c
siwei.xu <siwei.xu@sonymobile.com>
2014-08-11 11:16:59 +0800

    a little change for netlink_test.c

---
+++ netlink_test.c
@@ -0,0 +1,57 @@
+#include <linux/rtnetlink.h>
+#include <linux/netlink.h>
+#include <linux/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/un.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define UEVENT_BUFFER_SIZE 2048
+
+static int init_hotplug_sock(void) {
+	int ret;
+	int s = -1;
+	const int buffersize = 1024;
+	struct sockaddr_nl snl;
+
+	bzero(&snl, sizeof(struct sockaddr_nl));
+	snl.nl_family = AF_NETLINK;
+	snl.nl_pid = getpid();
+	snl.nl_groups = RTMGRP_LINK | RTMGRP_IPV4_IFADDR;
+
+	s = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_ROUTE); // NETLINK_KOBJECT_UEVENT
+	if (s == -1) {
+		perror("socket");
+		return -1;
+	}
+	setsockopt(s, SOL_SOCKET, SO_RCVBUF, &buffersize, sizeof(buffersize));
+
+	ret = bind(s, (struct sockaddr *) &snl, sizeof(struct sockaddr_nl));
+	if (ret < 0) {
+		perror("bind");
+		close(s);
+		return -1;
+	}
+
+	return s;
+}
+
+int main(int argc, char *argv[]) {
+	int hotplug_sock = init_hotplug_sock();
+
+	while (1) {
+		char buf[UEVENT_BUFFER_SIZE * 2] = { 0 };
+
+		recv(hotplug_sock, &buf, sizeof(buf), 0);
+		printf("%s\n", buf);
+	}
+
+	return 0;
+}
